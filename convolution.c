cb62684c8059b716b71d699234c2d941a478e7bc
Tom Castle <castlet1@wit.edu>
2019-07-25 21:33:28 -0400

    image convolution parallelized with omp! but not tested.

---
+++ convolution.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include "convolution.h"
+
+int *convolute_image(int *img, int *dims, double **kernel, int k) {
+    int a;
+    int *filtered;
+
+    filtered = (int *) malloc(dims[0] * dims[1] * sizeof(int));
+
+    #pragma omp parallel for shared(filtered)
+    for(int i = 0; i < dims[0]; i++) {
+        for(int j = 0; j < dims[1]; j++) {
+            a = 0.0;
+
+            for(int ki = -k; ki <= k; ki++) {
+                for(int kj = -k; kj <= k; kj++) {
+                    if((i + ki < 0) || (j + kj < 0) || (i + ki > dims[0] - 1) || (j + kj > dims[1] - 1)) {
+                        continue;
+                    } else {
+                        a += kernel[kj + 1][ki + 1] * img[(i + ki) * dims[1] + (j + kj)];
+                    }
+                }
+            }
+
+            if(a > 255) a = 255;
+            if(a < 0) a = 0;
+            filtered[(i * dims[1]) + j] = (int) a;
+        }
+    }
+
+    return filtered;
+}
