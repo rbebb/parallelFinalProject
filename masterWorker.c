0586e861aa77abecbd63194ded5dba4ceae303de
labelle <labelle@wit.edu>
2019-08-07 16:40:54 -0400

    removed convolution and made changes to masterWorker per comments

--- masterWorker.c
+++ masterWorker.c
@@ -23,22 +23,22 @@ int main(int argc, char** argv) {
 
 	int* mat;
 	int* dims;
-	int* answer
+	int* answer;
 
 	int done = 0;
 	int counter = 0;
 	char* file = (char*)malloc(20 * sizeof(char));
 	int x = rank;
 
-	if (myranks == 0) {
+	if (rank == 0) {
 
 
 		//openCV calls that get us all of our images for convolution
 		done = 0; //change from zero to number of images that need convolution
-		for (int i = 1; i < ranks; i++) {
+		for (int i = 1; i < numranks; i++) {
 			file = 'the counter index of our images'; //assume we have at least enough images for init
 			counter++;
-			MPI_Isend(file, 20, MPI_CHAR, i, 0, MPI_COMM_WORLD, req); //non-blocking so we can get on with sending the other file names for opening
+			MPI_Isend(file, 20, MPI_CHAR, i, 0, MPI_COMM_WORLD, &req); //non-blocking so we can get on with sending the other file names for opening
 		}
 
 		while (1) {
@@ -46,7 +46,7 @@ int main(int argc, char** argv) {
 			if (counter < done) {
 				file = 'the counter index of our images';
 				counter++;
-				MPI_Isend(file, 20, MPI_CHAR, x, 0, MPI_COMM_WORLD, req);//non-blocking so that we can get on with recv and sending
+				MPI_Isend(file, 20, MPI_CHAR, x, 0, MPI_COMM_WORLD, &req);//non-blocking so that we can get on with recv and sending
 			}
 			else {
 				int finished = 0;
@@ -60,7 +60,8 @@ int main(int argc, char** argv) {
 
 	}
 
-	if (myrank != 0) {
+	if (rank != 0) {
+		int* answer;
 		dims = (int*)malloc(sizeof(int) * 2);
 		while (1) {
 			MPI_Recv(file, 20, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &stat);//blocking because we cant do anything till we know what file to open
